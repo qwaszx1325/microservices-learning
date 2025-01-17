c98cff9ae4e76e8cca8c82168b82215e15e0190f
Xiu <qwaszx1325@gmail.com>
2025-01-17 12:40:58 +0800

    使用IDEA建立order和product集群,並寫一個測試程式讀取service名稱、ip和port

---
+++ services/service-product/src/test/java/com/xiu/DiscoveryTest.java
@@ -0,0 +1,31 @@
+package com.xiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+
+import java.util.List;
+
+@SpringBootTest
+public class DiscoveryTest {
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @Test
+    void discoveryClientTest(){
+        for (String service : discoveryClient.getServices()) {
+            System.out.println("service: " + service);
+
+            List<ServiceInstance> instances = discoveryClient.getInstances(service);
+
+            for (ServiceInstance instance : instances) {
+                System.out.println("ip" + instance.getHost() +
+                        ": port = " +instance.getPort() );
+
+
+            }
+        }
+    }
+}
