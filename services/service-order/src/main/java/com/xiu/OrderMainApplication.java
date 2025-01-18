413e53902e771ef09d22a6c2f01822636fe0595b
Xiu <qwaszx1325@gmail.com>
2025-01-19 06:44:33 +0800

    在spring boot建立監聽行為

--- services/service-order/src/main/java/com/xiu/OrderMainApplication.java
+++ services/service-order/src/main/java/com/xiu/OrderMainApplication.java
@@ -1,11 +1,39 @@
 package com.xiu;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class OrderMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderMainApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(NacosConfigManager nacosConfigManager){
+        return ags ->{
+            ConfigService configService = nacosConfigManager.getConfigService();
+            configService.addListener("service-order.properties", "DEFAULT_GROUP", new Listener() {
+                @Override
+                public Executor getExecutor() {
+                    return Executors.newFixedThreadPool(4);
+                }
+
+                @Override
+                public void receiveConfigInfo(String s) {
+                    System.out.println("變化的配置訊息:"+ s);
+                }
+            });
+            System.out.println("=====================");
+
+        };
     }
 }
